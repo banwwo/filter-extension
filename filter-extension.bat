7b3f3b8caaa9ecd67a0d51d2c20e57c48700cd33
Esteban Emanuel Fernandez Alfonso <56206284+estebannsz@users.noreply.github.com>
2022-09-12 02:31:14 -0300

    Create filter-extension.bat

---
+++ filter-extension.bat
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001
+
+@REM Settings
+set pwd="%HOMEPATH%"
+set target="%HOMEPATH%"
+set extension=*.jpg
+set secondExtension=*.mp4
+set thirdExtension=*.jpeg
+set fourthExtension=*.png
+
+echo.
+echo Script Settings...
+echo.
+echo [*] current directory: %pwd%
+echo [*] target directory: %target%
+echo [*] files with: %extension% %secondExtension% %thirdExtension% %fourthExtension%
+echo.
+
+:do
+copy /Y %pwd%\%extension% %target%
+copy /Y %pwd%\%secondExtension% %target%
+copy /Y %pwd%\%thirdExtension% %target%
+copy /Y %pwd%\%fourthExtension% %target%
